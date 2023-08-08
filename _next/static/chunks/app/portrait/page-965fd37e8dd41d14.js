ad3b6985210e3d378d57b9ee21641954d5fcca94
TikhonovMikhail <tixonovmisha@gmail.com>
2023-08-08 17:14:52 +0400

    Deploy

---
+++ _next/static/chunks/app/portrait/page-965fd37e8dd41d14.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[687],{9715:function(e,n,s){Promise.resolve().then(s.t.bind(s,6685,23)),Promise.resolve().then(s.bind(s,2955))}},function(e){e.O(0,[685,955,971,596,744],function(){return e(e.s=9715)}),_N_E=e.O()}]);
